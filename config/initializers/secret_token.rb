913f8478398bf8cfdd742f021979719fb4e0a3ac
caiguanhao <caiguanhao@gmail.com>
2013-06-28 20:16:09 +0800

    first commit: rails new EasyJobs -T (ruby 2.0/rails 4.0)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EasyJobs::Application.config.secret_key_base = 'b8ce093d1b8b8540566e0cc9f1e3f3d7b1416a44d734476346c86291d7e2fe4dd6633d1d00982302b99280aaf888c9546e62356ae43e3702b580ac0bc5aabe9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EasyJobs::Application.config.session_store :cookie_store, key: '_EasyJobs_session'
