c61791efadc057c217e97a30aca8952a25f92cbc
caiguanhao <caiguanhao@gmail.com>
2013-07-01 01:48:42 +0800

    set seed;

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 Interpreter.create([
+  { path: "/bin/bash",       upload_script_first: true  },
   { path: "/usr/bin/perl"  , upload_script_first: false },
   { path: "/usr/bin/php"   , upload_script_first: false },
-  { path: "/usr/bin/python", upload_script_first: false }
+  { path: "/usr/bin/python", upload_script_first: false },
+  { path: "/usr/bin/ruby",   upload_script_first: false },
 ])
