53a6bc00f779abe8e406c96af3d9ccd9c29beeb6
caiguanhao <caiguanhao@gmail.com>
2013-07-01 02:25:08 +0800

    add model validations;

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ActiveRecord::Base
   belongs_to :server
   belongs_to :interpreter
+  validates_presence_of :name, :script
 end
--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,4 @@
 class Server < ActiveRecord::Base
   has_many :jobs
+  validates_presence_of :name, :host, :username
 end
