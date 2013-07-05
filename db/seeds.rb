9073cf2b16d25678c20d625cdad33e8c0cf55e53
caiguanhao <caiguanhao@gmail.com>
2013-07-05 14:11:51 +0800

    add admin.create to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@ Interpreter.create([
   { path: "/usr/bin/python", upload_script_first: false },
   { path: "/usr/bin/ruby",   upload_script_first: false },
 ])
+Admin.create([
+  { username: "admin", email: "admin@example.com", password: "12345678" },
+])
