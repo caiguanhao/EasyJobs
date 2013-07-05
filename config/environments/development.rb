8583b1e996698fbff6b56d8772a370f4564139dc
caiguanhao <caiguanhao@gmail.com>
2013-07-05 16:47:03 +0800

    start translation;

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ EasyJobs::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.i18n.default_locale = 'zh-CN'
 end
