fb320a50979561d367458b8b40137a6df0760fa5
caiguanhao <caiguanhao@gmail.com>
2013-07-17 19:53:22 +0800

    add api v1 run page;

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::JobsController < Api::V1Controller
+  layout false
 
   def index
     render json: Job.joins('LEFT JOIN servers ON servers.id = jobs.server_id')
@@ -12,15 +13,20 @@ class Api::V1::JobsController < Api::V1Controller
       server = job.server.attributes
       server.delete("password")
     end
+    require 'digest/md5'
+    script_hash = Digest::MD5.hexdigest(job.script)
     render json: {
       job: job,
       interpreter: job.interpreter,
-      server: server
+      server: server,
+      hash: script_hash
     }
   end
 
   def run
-    render json: {}
+    p = params.permit(:token, :hash, :exit_if_non_zero, :parameters).to_query
+    p = "?" + p unless p.empty?
+    @eventSource = run_job_path(params[:job_id]) + p
   end
 
 end
